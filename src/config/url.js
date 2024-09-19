b0dd1b96992358e7fdd7de4d42626279d5aa9ab7
sayedmohdanas <mohdanas9272@gmail.com>
2024-09-19 10:42:10 +0530

    wip

--- src/config/url.js
+++ src/config/url.js
@@ -1,6 +1,6 @@
 // const API_BASE_URL = 'http://192.168.29.189:4000/'  
 
-// const API_BASE_URL = 'http://192.168.29.238:9292/' 
+// const API_BASE_URL = 'http://192.168.29.244:5000/'
 const API_BASE_URL ='http://52.66.236.213:9292/'
 // 
 const getEndpoint = text => {
