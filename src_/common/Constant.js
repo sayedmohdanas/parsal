47f40b73424c37e950cf75ee534c0f092c3021bf
sayedmohdanas <mohdanas9272@gmail.com>
2024-11-05 12:45:34 +0530

    delete_older_src

--- src_/common/Constant.js
+++
@@ -1,6 +0,0 @@
-export const OrderButton = [
-    'All',
-    'On the way',
-    'Shipped',
-    'Cancelled',
-]
--- src_/common/Font.js
+++
@@ -1,9 +0,0 @@
-const txt_bold = "Inter_28pt-SemiBold";
-const normal_txt = "Inter_28pt-Regular"
-const medium_txt = "Inter_28pt-Medium"
-
-export default {
-  txt_bold,
-  normal_txt,
-  medium_txt
-};
